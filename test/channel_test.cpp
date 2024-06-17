b9a63086ee66aa0e1d3dfcc2df99a8c802160c3d
chunlin.yang <yangchunlin10061140@gmail.com>
2024-06-17 16:18:37 +0000

    channel unittest

--- test/channel_test.cpp
+++ test/channel_test.cpp
@@ -19,4 +19,27 @@ constexpr static inline size_t length = 1024 * 1024;
 
 TEST(Shm, T0)
 {
+	int length = 10;
+	extra::kernel::Channel<Order> chan("chunlin", 0, length + 1);
+	bool succ = chan.create();
+	EXPECT_EQ(succ, true);
+
+	for(int i=0;i<length;i++){
+		std::cout << "write iterator " << i << std::endl;
+		auto it = chan.write_iterator();
+		it->id = i;
+		std::cout << "write iterator id " << it->id << std::endl;
+		strcpy(it->name, "hello");
+		it->volume = i;
+		it->price = i;
+	}
+
+	auto it_r = chan.read_iterator();
+	int expected_id = 0; 
+	while(it_r.next()) {
+		std::cout << "read result " << it_r->id << std::endl;
+		EXPECT_EQ(it_r->id, expected_id);
+		expected_id += 1;
+	}
+	EXPECT_EQ(length, expected_id);
 }
